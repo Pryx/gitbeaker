ed34c6e589f94e1b2c8ef4488275fd351fe1c984
Justin <justin.s.dalrymple@gmail.com>
2021-03-14 16:56:16 -0400

    Switch to playwright

--- packages/gitbeaker-browser/test/integration/index.ts
+++ packages/gitbeaker-browser/test/integration/index.ts
@@ -1,4 +1,24 @@
 import * as path from 'path';
+import { chromium, Browser, Page } from "playwright";
+
+let browser: Browser;
+let page: Page;
+
+beforeAll(async () => {
+  browser = await chromium.launch();
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+afterEach(async () => {
+  await page.close();
+});
 
 describe('Browser Import', () => {
   it('should import the Gitbeaker library though the global gitbeaker', async () => {
